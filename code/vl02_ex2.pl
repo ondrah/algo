92131d61c18c036c799ed007e14ac4d57916d0e0
ond <ondra.havel@gmail.com>
2019-12-10 00:19:03 +0100

    adding vl02_ex2.pl

---
+++ code/vl02_ex2.pl
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/perl
+# A short Perl solution using regular expression.
+# First, every group (0|1) is matched. See the /g modifier, match globally.
+# For each match a code is executed, see the /e modifier, execute code.
+# Is the group match equal to '0', '1' is used as a replacement,
+# '10' otherwise.
+
+$count = 11;
+$e = '0';
+while($count-- > 0) {
+	print length $e, " ", $e, "\n";
+	$e =~ s/(0|1)/$1 eq '0' ? '1' : '10'/ge;
+}
